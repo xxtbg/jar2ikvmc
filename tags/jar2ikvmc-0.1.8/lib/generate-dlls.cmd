6659c041fe834a43dd93ae95bbdf595baae1f54c
Gennadiy.Donchyts <Gennadiy.Donchyts@1ae3c2c0-8d3d-0410-a649-71cf14905ac9>
2009-03-22 16:16:27 +0000

    Release jar2ikvmc-0.1.8

---
+++ tags/jar2ikvmc-0.1.8/lib/generate-dlls.cmd
@@ -0,0 +1,3 @@
+ikvm-0.38.0.2\ikvmc jar\bcel-5.2.jar -target:library
+ikvm-0.38.0.2\ikvmc jar\jakarta-regexp-1.3.jar -target:library
+ikvm-0.38.0.2\ikvmc jar\jaranalyzer-1.2.jar -r:jakarta-regexp-1.3.dll -r:bcel-5.2.dll -target:library
